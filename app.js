04332cf865b577699dd9a78e55b90b65489844d0
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2025-03-25 13:37:49 +0300

    lasson seven

---
+++ app.js
@@ -0,0 +1,41 @@
+let darkMode = document.querySelector("#darkMode");
+let body = document.body;
+
+const getThemeUse = () => {
+  let theme = localStorage.getItem("theme");
+
+  if (theme === null) {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  } else {
+    setTheme(theme);
+  }
+};
+
+const setTheme = (theme) => {
+  if (theme === "dark") {
+    body.classList.add("dark-mode");
+    darkMode.classList.replace("fa-moon", "fa-sun");
+  } else {
+    body.classList.remove("dark-mode");
+    darkMode.classList.replace("fa-sun", "fa-moon");
+  }
+};
+
+getThemeUse();
+
+darkMode.addEventListener("click", () => {
+  if (darkMode.classList[1] === "fa-moon") {
+    setTheme("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    setTheme("light");
+    localStorage.setItem("theme", "light");
+  }
+});
